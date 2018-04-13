5c5ac6688df42fd85a72626c8a2aba2baaaf9f33
Natalia Stafeeva <n.stafeeva@gmail.com>
2018-04-13 11:02:39 +0100

    Add about page

---
+++ pages/about.jsx
@@ -0,0 +1,6 @@
+export default () => (
+  <div>
+    <h1>About UM fashion project</h1>
+    <p>Narwhal asymmetrical hoodie, ramps deep v tattooed raw denim godard farm-to-table actually YOLO chambray celiac fixie raclette. Copper mug asymmetrical 8-bit pickled farm-to-table +1 flexitarian shabby chic tacos. Seitan typewriter brooklyn celiac. Bicycle rights austin fingerstache flexitarian hexagon cardigan portland lumbersexual cornhole tattooed +1 deep v. Umami pinterest twee before they sold out actually la croix hella af poutine tacos asymmetrical craft beer lyft vinyl. Lo-fi sartorial subway tile health goth brooklyn keytar, craft beer direct trade. Cloud bread pabst actually, quinoa coloring book 8-bit portland cliche gentrify hella twee lomo tofu.</p>
+  </div>
+)
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a style={{ fontSize: 20 }}>About Page</a>
+    </Link>
     <h1>Welcome to UM Fashion Blog</h1>
     <p>Fashion is about dressing according to what's fashionable. Style is more about being yourself. There is always an emotional element to anything that you make. I always say: To be well dressed you must be well naked. I do not have one theme for each season, I just try to make beautiful clothes all year round. I do not have one theme for each season, I just try to make beautiful clothes all year round.</p>
   </div>
