6edaeb901c36699402b87f0061898a2ace028240
Natalia Stafeeva <n.stafeeva@gmail.com>
2018-04-13 10:50:24 +0100

    Create a project

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+const Index = () => (
+  <div>
+    <h1>Welcome to UM Fashion Blog</h1>
+    <p>Fashion is about dressing according to what's fashionable. Style is more about being yourself. There is always an emotional element to anything that you make. I always say: To be well dressed you must be well naked. I do not have one theme for each season, I just try to make beautiful clothes all year round. I do not have one theme for each season, I just try to make beautiful clothes all year round.</p>
+  </div>
+)
+
+export default Index
