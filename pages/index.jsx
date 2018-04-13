993d44789851844026744b7ff280a20ebb277593
Natalia Stafeeva <n.stafeeva@gmail.com>
2018-04-13 11:52:38 +0100

    Add posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,22 @@
+import Layout from '../components/MyLayout'
 import Link from 'next/link'
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a style={{ fontSize: 20 }}>About Page</a>
+const PostLink = (props) => (
+  <li>
+    <Link as={`/posts/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
     </Link>
-    <h1>Welcome to UM Fashion Blog</h1>
-    <p>Fashion is about dressing according to what's fashionable. Style is more about being yourself. There is always an emotional element to anything that you make. I always say: To be well dressed you must be well naked. I do not have one theme for each season, I just try to make beautiful clothes all year round. I do not have one theme for each season, I just try to make beautiful clothes all year round.</p>
-  </div>
+  </li>
 )
 
-export default Index
+export default () => (
+  <Layout>
+    <h1>Welcome to UM Fashion Blog</h1>
+    <p>Fashion is about dressing according to what's fashionable. Style is more about being yourself. There is always an emotional element to anything that you make. I always say: To be well dressed you must be well naked. I do not have one theme for each season, I just try to make beautiful clothes all year round. I do not have one theme for each season, I just try to make beautiful clothes all year round.</p>
+    <ul>
+      <PostLink id="ums-best" title="UM's best" />
+      <PostLink id="product-team" title="Product Team's Fashion Corner" />
+      <PostLink id="party" title="Party Looks" />
+    </ul>
+  </Layout>
+)
---
+++ pages/post.jsx
@@ -0,0 +1,8 @@
+import Layout from '../components/MyLayout'
+
+export default (props) => (
+  <Layout>
+    <h1>{props.url.query.title}</h1>
+    <p>Luxury must be comfortable, otherwise it is not luxury. Age is something only in your head or a stereotype. Age means nothing when you are passionate about something. Fashion should be fun. It shouldn't be labelled intellectual. Comfort is very important to me. I think people live better in big houses and in big clothes. It's sometimes said that I'm rebellious and I do things to push people's buttons, but I just like the challenge.</p>
+  </Layout>
+)
